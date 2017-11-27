979c270d6c6bd91686ca81596b2829f460a906e6
Angelo Frangione <angelo.frangione@gmail.com>
2017-11-27 16:17:19 +0100

    Relu+fc1

--- models.py
+++ models.py
@@ -5,9 +5,11 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc0 = nn.Linear(28*28, 27)
+        self.fc0 = nn.Linear(28*28, 28*28)
+        self.fc1 = nn.Linear(28*28, 27)
 
     def forward(self, x):
         x = self.fc0(x.view(x.size(0), -1))
-        
+        F.relu(x)
+        x = self.fc1(x.view(x.size(0), -1))
         return F.log_softmax(x)
