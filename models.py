c0ef782284aefcfa8e19ff69bd6d17e0c4c35213
Angelo Frangione <angelo.frangione@gmail.com>
2017-11-27 16:42:39 +0100

    upgrade

--- models.py
+++ models.py
@@ -7,9 +7,12 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.fc0 = nn.Linear(28*28, 28*28)
         self.fc1 = nn.Linear(28*28, 27)
+        # self.fc2 = nn.Linear(20*20, 27)
 
     def forward(self, x):
         x = self.fc0(x.view(x.size(0), -1))
-        F.relu(x)
+        x = F.relu(x)
         x = self.fc1(x.view(x.size(0), -1))
+        # F.sigmoid(x)
+        # x = self.fc2(x.view(x.size(0), -1))
         return F.log_softmax(x)
